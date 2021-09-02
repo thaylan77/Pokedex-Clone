bfe7a9e4730212a9cc888fad3e0e8c83351ec5fd
thaylan77 <thaylancavalcante@gmail.com>
2021-09-02 08:18:30 -0300

    150 first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from './containers/PokemonContainer';
+import './App.css';
+
+function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app'
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  );
+}
+
+export default App;
